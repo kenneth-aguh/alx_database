5bd714717665f9338e1916b00757a534be41cf9c
aguh kenneth <kennethpro2012@gmail.com>
2024-01-23 18:54:25 +0100

    commit

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+--  a script to show all privileges of the MySQL users user_0d_1 and user_0d_2 on the localhost
+
+-- Grants held by user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Grants held user_0d_2
+
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
